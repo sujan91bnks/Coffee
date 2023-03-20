b74b0befb991755dda612db22320535d30784c1f
Sujan Thapa <10thapasujan91@gmail.com>
2023-03-19 21:32:46 -0400

    withpopup

--- script.js
+++ script.js
@@ -3,5 +3,21 @@ function buy(){
 }
 
 const $ = document.querySelector.bind(document);
-const clickBtn = $('#coffee-item');
-clickBtn.addEventListener('click',buy);
+const titleBTn = $('#leftButton');
+const changeHeader = $('#header');
+const pop = $('#popUp');
+
+titleBTn.addEventListener('click', foo);
+changeHeader.addEventListener('click', ()=>{console.log("clicked on Header")});
+
+function foo(){
+        titleBTn.classList.add('glassOnion');
+        //titleBTn.classList.remove() This function removes the class name and returns function in initial form
+        console.log("Button clicked");
+        changeHeader.style.background = 'white';
+        pop.style.display = 'block';
+
+}
+   
+//const clickBtn = $('.coffee-item');
+// clickBtn.addEventListener('click',buy);
